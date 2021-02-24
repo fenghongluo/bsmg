7de59716fc4f762fba9ae905728290f0c89bd89c
张建伟 <jianwei.zhang@nvr.cn>
2021-02-24 14:50:47 +0800

    首次提交

---
+++ src/main/java/com/bsmg/BsmgApplication.java
@@ -0,0 +1,13 @@
+package com.bsmg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BsmgApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BsmgApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bsmg/BsmgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bsmg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BsmgApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
